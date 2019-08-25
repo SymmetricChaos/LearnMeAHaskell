7b950f4020e5ec85839e5b7978325363a1e782ad
SymmetricChaos <ajfraebel@gmail.com>
2019-08-24 21:43:00 -0400

    Update basicExamples.hs

--- basicExamples.hs
+++ basicExamples.hs
@@ -2,5 +2,13 @@ lucky :: (Integral a) => a -> String
 lucky 7 = "LUCKY!"
 lucky x = "unlucky"
 
-removeLower :: [Char] -> [Char]
+removeLower :: String -> String
 removeLower st = [c | c <- st, c `elem` ['A'..'Z']]
+
+double :: Integer -> Integer
+double x = 2*x
+
+-- Simple function to generate primes
+primes = [ n | n <- [2..], all ((> 0) . rem n) [2..(n-1)] ]
+
+primesDouble = map double (take 20 primes)
