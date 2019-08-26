e04e99e97aa18dddf5066cd6f724d791c297e7ad
SymmetricChaos <ajfraebel@gmail.com>
2019-08-25 21:52:47 -0400

    Update ProjectEuler.hs

--- ProjectEuler.hs
+++ ProjectEuler.hs
@@ -1,6 +1,10 @@
+-- These aren't necessarily solutions to the exact questions asked 
+
 import Data.List (union, sort)
+-- Numbers under 1000 that are divisible by 3 or 5 (or both) without repetition
 problem_1 = sort (union [3,6..999] [5,10..999])
 
+-- Even fibonnaci numbers less than 1000000
 problem_2 = [ x | x <- takeWhile (<= 1000000) fibs, even x]
   where
     fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
