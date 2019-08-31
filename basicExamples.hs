c96dde0f0c5f547a41192d9548e4c5d080a94e59
SymmetricChaos <ajfraebel@gmail.com>
2019-08-31 08:18:17 -0400

    more better

--- basicExamples.hs
+++ basicExamples.hs
@@ -11,18 +11,27 @@ double x = 2*x
 -- Simple function to generate primes
 primes = [ n | n <- [2..], all ((> 0) . rem n) [2..(n-1)] ]
 
-primesDouble = map double (take 20 primes)
-
 some_string = "iuMowrhjYqwSpoqEeCqweRvbdEfThg"
+
+my_max :: (Ord a) => [a] -> a
+my_max [] = error "empty list"
+my_max [x] = x
+my_max (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = my_max xs
+
 
 main :: IO()
 main =    
     mapM_
     putStrLn
-    ["Show the double of the first 20 primes"
-    , show $ primesDouble
-    , "\nSome text"
+    ["map double (take 20 primes)"
+    , show $ map double (take 20 primes)
+    , "\nsome_string"
     , show $ some_string
-    , "Now with lower case removed"
+    , "removeLower some_string"
     , show $ removeLower some_string
+    , "\nzipWith (*) [1,2,3] [2,4,6]"
+    , show $ zipWith (*) [1,2,3] [2,4,6]
     ] 
