6764e6a9584173b840da7ff2d39ed8e20c9bd507
SymmetricChaos <ajfraebel@gmail.com>
2019-09-04 18:34:28 -0400

    Update Lsystem.hs

--- Lsystem.hs
+++ Lsystem.hs
@@ -1,10 +1,21 @@
+-- An L-sytem rule
 rule :: Char -> String
 rule 'A' = "AB"
 rule 'B' = "A"
 
 
-
+-- Crude L-sytem rule I made
 l_system :: String -> [String]
 l_system "" = [""]
 l_system (x:xs) = rule x : l_system xs
 
+
+-- Using concatMap
+main :: IO()
+main =    
+    mapM_
+    putStrLn
+    [show $ concatMap rule "A"
+    , show $ concatMap rule "AB"
+    , show $ concatMap rule "ABA"
+    ]
