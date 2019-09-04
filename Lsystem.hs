6d6158abb1424cb8e2f37c88bd661da866b58c1f
SymmetricChaos <ajfraebel@gmail.com>
2019-09-04 18:17:18 -0400

    Update Lsystem.hs

--- Lsystem.hs
+++ Lsystem.hs
@@ -1,8 +1,10 @@
 rule :: Char -> String
-rule "A" = "AB"
-rule "B" = "A"
-rule x = x
+rule 'A' = "AB"
+rule 'B' = "A"
+
+
 
 l_system :: String -> [String]
-l_system "" = error "empty string"
+l_system "" = [""]
 l_system (x:xs) = rule x : l_system xs
+
