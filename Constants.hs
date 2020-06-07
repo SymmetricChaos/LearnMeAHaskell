0c3361d1f39f7e48312f3f6361d85ee1078c04b3
SymmetricChaos <ajfraebel@gmail.com>
2020-06-07 09:24:18 -0400

    stuff

---
+++ Constants.hs
@@ -0,0 +1,17 @@
+import Numeric.MathFunctions.Constants
+
+main = do
+    print m_epsilon
+    print m_huge
+    print m_tiny
+    print m_max_exp
+    print m_pos_inf
+    print m_neg_inf
+    print m_NaN
+
+    print m_1_sqrt_2
+    print m_2_sqrt_pi
+    print m_ln_sqrt_2_pi
+    print m_sqrt_2
+    print m_sqrt_2_pi
+    print m_eulerMascheroni
